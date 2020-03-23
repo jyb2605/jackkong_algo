b6a90257b8ba1de0687f958095e4363ac4602262
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-03-23 15:14:03 +0900

    Improve fourth questiong

---
+++ 20200322/jaeyeonlee0621_4.py
@@ -0,0 +1,23 @@
+def solution(priorities, location):
+    len_ = len(priorities)
+    stack = [0 for _ in range(0, 10)]
+    for i in range(0, len_):
+        stack[priorities[i]] += 1
+    start = 0
+    count = 0
+    stop = False
+    for i in range(9, -1, -1):
+        if stop: break
+        while stack[i]:
+            if priorities[start] == i:
+                count += 1
+                stack[i] -= 1
+                if start == location:
+                    stop = True
+                    break
+            start = (start + 1) % len_
+    return count
+
+
+print(solution([2, 1, 3, 2], 2))
+print(solution([1, 1, 9, 1, 1, 1], 0))
