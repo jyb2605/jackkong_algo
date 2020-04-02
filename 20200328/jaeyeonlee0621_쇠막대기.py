490a20a331cebc6c519fe4d231d4b61b7413ada0
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-04-02 13:31:35 +0900

    Imprve best answer (다른 사람..)

--- 20200328/jaeyeonlee0621_쇠막대기.py
+++ 20200328/jaeyeonlee0621_쇠막대기.py
@@ -15,3 +15,17 @@ def solution(arrangement):
 
 
 print(solution('()(((()())(())()))(())'))
+
+'''
+def solution(arrangement):
+    answer = 0
+    sticks = 0
+    rasor_to_zero = arrangement.replace('()','0')
+    for i in rasor_to_zero:
+        if i == '(': sticks += 1
+        elif i =='0' : answer += sticks
+        else :
+            sticks -= 1
+            answer += 1
+    return answer
+'''
