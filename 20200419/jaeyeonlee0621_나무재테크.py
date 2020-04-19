5031b7b528327095819aecacde695d491ef03fa2
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-04-19 13:07:57 +0900

    Improve 난 삼성가긴 글렀다

---
+++ 20200419/jaeyeonlee0621_나무재테크.py
@@ -0,0 +1,77 @@
+'''
+봄
+- 자신의 나이만큼 양분을 먹고 나이가 1씩 증가
+- 나무는 자신이 있는 곳의 양분만 먹을 수 있음
+- 여러개의 나무가 있다면 나이가 어린 나무부터 양분을 먹음
+- 양분을 먹을 수 없다면 죽음
+
+여름
+- 봄에 죽은 나무가 양분으로 변함
+- 죽은 나무는 나이로 2로 나눈값이 양분으로 추가 > 소수점 아래는 버린다
+
+가을
+- 나무가 번식
+- 번식하는 나무는 나이가 5의 배수여야 한다
+- 인접한 8개의 칸에 나이가 1인 나무가 생긴다
+
+겨울
+- 양분을 추가해줌
+- 양분이 추가되는 양은 입력으로 주어짐
+'''
+
+import sys
+
+answer = 0
+
+n, m, k = map(int, sys.stdin.readline().split())
+land = [[5 for _ in range(101)] for _ in range(101)]
+nourishment = []
+tree = [[[] for _ in range(101)] for _ in range(101)]
+dead_tree = []
+
+for x in range(n): nourishment.append(list(map(int, sys.stdin.readline().split())))
+
+for _ in range(m):
+    x, y, year = map(int, sys.stdin.readline().split())
+    tree[x - 1][y - 1].append(year)
+    tree[x - 1][y - 1].sort(reverse=True)
+
+for _ in range(k):
+    dead_tree = []
+
+    # 봄
+    for x in range(101):
+        for y in range(101):
+            for index in range(len(tree[x][y])-1, -1, -1):
+                if land[x][y] < tree[x][y][index]:
+                    for i in range(index, -1, -1): dead_tree.append((x, y, tree[x][y][i]))
+                    tree[x][y] = tree[x][y][index + 1:]
+                    break
+                land[x][y] -= tree[x][y][index]
+                tree[x][y][index] += 1
+
+    # 여름
+    for x, y, year in dead_tree: land[x][y] += int(year / 2)
+
+    # 가을
+    for x in range(101):
+        for y in range(101):
+            for i in range(len(tree[x][y])):
+                if tree[x][y][i] % 5 == 0:
+                    if x - 1 >= 0 and y - 1 >= 0: tree[x - 1][y - 1].append(1)
+                    if x - 1 >= 0: tree[x - 1][y].append(1)
+                    if x - 1 >= 0 and y + 1 < 101: tree[x - 1][y + 1].append(1)
+                    if y - 1 >= 0: tree[x][y - 1].append(1)
+                    if y + 1 < 101: tree[x][y + 1].append(1)
+                    if x + 1 < 101 and y - 1 >= 0: tree[x + 1][y - 1].append(1)
+                    if x + 1 < 101: tree[x + 1][y].append(1)
+                    if x + 1 < 101 and y + 1 < 101: tree[x + 1][y + 1].append(1)
+
+    # 겨울
+    for x in range(n):
+        for y in range(n): land[x][y] += nourishment[x][y]
+
+for x in range(101):
+    for y in range(101): answer += len(tree[x][y])
+
+print(answer)
