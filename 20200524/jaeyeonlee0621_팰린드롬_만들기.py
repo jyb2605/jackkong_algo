4629e33bf23fe0e31178af12c239ea17f8791cb3
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-05-26 21:57:20 +0900

    ..?

--- 20200524/jaeyeonlee0621_팰린드롬_만들기.py
+++ 20200524/jaeyeonlee0621_팰린드롬_만들기.py
@@ -1,40 +1,35 @@
-import sys
 import string
+import copy
 
 
 def solution(name):
-    alphabet = [0 for _ in range(27)]
-    name = name[:-1]
+    alphabet = [0 for _ in range(26)]
 
     for n in name:
         alphabet[string.ascii_uppercase.index(n)] += 1
 
-    answer = []
-    middle = ''
-    count = 0
-    for index in range(27):
+    middle = []
+    word = []
+    for index in range(26):
         if alphabet[index] % 2 == 1:
-            if count > 1:
-                return "I'm Sorry Hansoo"
-            count += 1
-            middle = string.ascii_uppercase[index]
+            middle.append(string.ascii_uppercase[index])
             alphabet[index] -= 1
-
-    for index in range(27):
-        if alphabet[index] > 0:
-            answer.append(string.ascii_uppercase[index])
+        if alphabet[index] > 0 and alphabet[index] % 2 == 0:
+            word.append(string.ascii_uppercase[index])
+    if len(middle) > 1:
+        return "I'm Sorry Hansoo"
+    answer = copy.deepcopy(word)
     if middle:
-        answer.append(middle)
-    for index in range(26, -1, -1):
-        if alphabet[index] > 0:
-            answer.append(string.ascii_uppercase[index])
+        answer.append(middle[0])
+    for index in range(len(word) - 1, -1, -1):
+        answer.append(word[index])
     return ''.join(answer)
 
 
-name = sys.stdin.readline()
+name = input()
 print(solution(name))
 
-# print(solution('AABB'))
+# print(solution('AAAB'))
 # print(solution('AACCCBB'))
 # print(solution('AADDDCCCBB'))
 # print(solution('ACABCB'))
