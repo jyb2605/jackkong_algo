2a27dfdec4f5acf8e2440cebca84dd98eb519c28
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-05-31 16:54:10 +0900

    Improve tomato

--- 20200531/jaeyeonlee0621_토마토.py
+++ 20200531/jaeyeonlee0621_토마토.py
@@ -1,42 +1,45 @@
+'''
+백준 7576
+queue 는 collections의 deque를 이용해야 시간초과를 해결할 수 있다
+'''
 import sys
+
+from collections import deque
 
 row, col = map(int, sys.stdin.readline().split())
 
 box = [[] for _ in range(col)]
+queue = deque()
 all_ = 0
+
 for y in range(col):
     box[y] = list(map(int, sys.stdin.readline().split()))
     for x in range(row):
         if box[y][x] != 0: all_ += 1
+        if box[y][x] == 1: queue.append((y, x, 0))
 
-day = 0
-while all_ < row * col:
-    day += 1
-    change = []
-    for y in range(col):
-        for x in range(row):
-            if box[y][x] == 1:
-                visited = [[0 for _ in range(row)] for _ in range(col)]
-                queue = [(y, x)]
-                while queue:
-                    y_, x_ = queue.pop(0)
-                    if not visited[y_][x_]:
-                        visited[y_][x_] = 1
-                        if y_ - 1 >= 0:
-                            if box[y_ - 1][x_] == 1: queue.append((y_ - 1, x_))
-                            if box[y_ - 1][x_] == 0: change.append((y_ - 1, x_))
-                        if y_ + 1 < col:
-                            if box[y_ + 1][x_] == 1: queue.append((y_ + 1, x_))
-                            if box[y_ + 1][x_] == 0: change.append((y_ + 1, x_))
-                        if x_ - 1 >= 0:
-                            if box[y_][x_ - 1] == 1: queue.append((y_, x_ - 1))
-                            if box[y_][x_ - 1] == 0: change.append((y_, x_ - 1))
-                        if x_ + 1 < row:
-                            if box[y_][x_ + 1] == 1: queue.append((y_, x_ + 1))
-                            if box[y_][x_ + 1] == 0: change.append((y_, x_ + 1))
-    for y, x in change:
-        if box[y][x] == 0:
-            box[y][x] = 1
-            all_ += 1
+day_ = 0
+while queue:
+    y_, x_, day = queue.popleft()
+    day_ = max(day_, day)
+    if y_ - 1 >= 0 and box[y_ - 1][x_] == 0:
+        all_ += 1
+        box[y_ - 1][x_] = 1
+        queue.append((y_ - 1, x_, day + 1))
+    if y_ + 1 < col and box[y_ + 1][x_] == 0:
+        all_ += 1
+        box[y_ + 1][x_] = 1
+        queue.append((y_ + 1, x_, day + 1))
+    if x_ - 1 >= 0 and box[y_][x_ - 1] == 0:
+        all_ += 1
+        box[y_][x_ - 1] = 1
+        queue.append((y_, x_ - 1, day + 1))
+    if x_ + 1 < row and box[y_][x_ + 1] == 0:
+        all_ += 1
+        box[y_][x_ + 1] = 1
+        queue.append((y_, x_ + 1, day + 1))
 
-print(day)
+if all_ == row * col:
+    print(day_)
+else:
+    print(-1)
