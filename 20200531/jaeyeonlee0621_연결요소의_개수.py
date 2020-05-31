91c0c7bcdc64470e89f7fefe16858161e76816ae
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-05-31 14:53:42 +0900

    Improve 11724

--- 20200531/jaeyeonlee0621_연결요소의_개수.py
+++ 20200531/jaeyeonlee0621_연결요소의_개수.py
@@ -1,3 +1,9 @@
+'''
+백준 11724
+BFS보다 DFS가 더 시간이 빠르다
+BFS는 모든 node를 방문하고 DFS는 일정 node는 방문하지 않기 때문이다
+'''
+
 import sys
 
 node, vertex = map(int, sys.stdin.readline().split())
@@ -10,17 +16,35 @@ for _ in range(vertex):
     map_[start].append(end)
     map_[end].append(start)
 
+# BFS
+# answer = 0
+# queue = []
+#
+# for i in range(1, node + 1):
+#     if not visited[i]:
+#         visited[i] = 1
+#         queue = map_[i]
+#         while queue:
+#             num = queue.pop(0)
+#             if not visited[num]:
+#                 queue.extend(map_[num])
+#                 visited[num] = 1
+#         answer += 1
+#
+# print(answer)
+
+
 answer = 0
-queue = []
+stack = []
 
 for i in range(1, node + 1):
     if not visited[i]:
         visited[i] = 1
-        queue = map_[i]
-        while queue:
-            num = queue.pop(0)
+        stack = map_[i]
+        while stack:
+            num = stack.pop()
             if not visited[num]:
-                queue.extend(map_[num])
+                stack.extend(map_[num])
                 visited[num] = 1
         answer += 1
 
