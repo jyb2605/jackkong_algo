3330724a7f6c69c7662cc26ba436ceb717270fd9
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-06-27 16:54:19 +0900

    12015 가장긴증가하는부분수열

---
+++ 20200627/jaeyeonlee0621_12015.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(sys.stdin.readline())
+list_ = list(map(int, sys.stdin.readline().split()))
+stack = [list_[0], ]
+
+for i in range(1, n):
+    if stack[-1] < list_[i]:
+        stack.append(list_[i])
+    else:
+        start = 0
+        end = len(stack)
+        while start < end:
+            mid = (start + end) // 2
+            if stack[mid] >= list_[i]: end = mid
+            else: start = mid + 1
+        stack[end] = list_[i]
+
+print(len(stack))
