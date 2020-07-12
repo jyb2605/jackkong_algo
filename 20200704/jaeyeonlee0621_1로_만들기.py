4f8dcf50f00a318f1f8f2fb63d2b0bca8b689d08
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-07-12 15:49:03 +0900

    1로 만들기

--- 20200704/jaeyeonlee0621_1로_만들기.py
+++ 20200704/jaeyeonlee0621_1로_만들기.py
@@ -20,3 +20,16 @@ if n - 1 >= 1:
     num3 = dp(n-1, count + 1)
 map_[num] = min(num1, num2, num3)
 '''
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0 for _ in range(n + 1)]
+
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+        dp[i] = dp[i // 2] + 1
+    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+        dp[i] = dp[i // 3] + 1
+
+print(dp[n])
