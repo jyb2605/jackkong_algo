ae3d0d7cfdcd0c7b1b914c5c42411958aaf0938d
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-07-19 11:19:48 +0900

    wine:

--- 20200712/jaeyeonlee0621_포도주_시식.py
+++ 20200712/jaeyeonlee0621_포도주_시식.py
@@ -1,28 +1,17 @@
 '''
-dp 진짜 어렵다
-이런 생각들을 어떻게 하는거지
+1. 먹음 + 다음 것도 먹음 + 한 칸 띄고 + dp
+2. 안먹음 = 그 전꺼 들고옴
+3. 먹음 + 다음 것 안 먹음 + dp
 '''
 import sys
 
 n = int(sys.stdin.readline())
 
-wine = []
-for i in range(n): wine.append(int(sys.stdin.readline()))
+wine = [0 for _ in range(n + 3)]
+for i in range(n): wine[i] = int(sys.stdin.readline())
+dp = [0 for _ in range(n + 3)]
 
-if n == 1:
-    print(wine[0])
-else:
-    dp = [0 for _ in range(n)]
-    dp[0] = wine[0]
-    dp[1] = wine[0] + wine[1]
+for i in range(n - 1, -1, -1):
+    dp[i] = max(wine[i] + wine[i + 1] + dp[i + 3], dp[i + 1], wine[i] + dp[i + 2])
 
-    '''
-    내 차례에 가질 수 있는 경우의 수는 3가지 이다.
-    1. 지금 와인 X
-    2. 지금 와인 O, 이전 와인 X
-    3. 지금 와인 O, 이전 와인 O
-    '''
-    for i in range(2, n):
-        dp[i] = max(dp[i - 1], wine[i] + dp[i - 2], wine[i] + wine[i - 1] + dp[i - 3])
-
-    print(dp[n - 1])
+print(dp[0])
