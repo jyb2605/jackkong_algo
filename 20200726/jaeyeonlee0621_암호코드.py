f31436582212ae35a7cc23a8c75c14224bca6139
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-07-26 16:58:36 +0900

    왜 틀렸어어어어엉어어어어

--- 20200726/jaeyeonlee0621_암호코드.py
+++ 20200726/jaeyeonlee0621_암호코드.py
@@ -1,5 +1,6 @@
 '''
 틀려씀ㅜㅜ
+왜...
 '''
 import sys
 
@@ -9,11 +10,10 @@ dp = [0 for _ in range(len(number) + 1)]
 dp[-1] = 1
 
 for i in range(len(number) - 1, -1, -1):
-    if number[i] == 0:
-        continue
-    if i + 1 < len(number) and 1 <= (number[i] * 10 + number[i + 1]) <= 26:
-        dp[i] = (dp[i + 1] + dp[i + 2]) % 1000000
-    else:
-        dp[i] = dp[i + 1]
+    if i + 1 < len(number) and 10 <= (number[i] * 10 + number[i + 1]) <= 26:
+        dp[i] += dp[i + 2]
+    if number[i] != 0:
+        dp[i] += dp[i + 1]
+    dp[i] %= 1000000
 
 print(dp[0])
