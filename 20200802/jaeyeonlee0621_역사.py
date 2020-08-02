c63a1b4fce05afd7a52896b26de3e5047f25b865
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-08-02 16:44:55 +0900

    history

---
+++ 20200802/jaeyeonlee0621_역사.py
@@ -0,0 +1,26 @@
+import sys
+
+event, history = map(int, sys.stdin.readline().split())
+
+map_ = [[0 for _ in range(event)] for _ in range(event)]
+
+for _ in range(history):
+    start, end = map(int, sys.stdin.readline().split())
+    map_[start - 1][end - 1] = -1
+    map_[end - 1][start - 1] = 1
+
+for i in range(event):
+    for y in range(event):
+        for x in range(event):
+            if y == i or x == i:
+                continue
+            if map_[y][i] == 1 and map_[i][x] == 1:
+                map_[y][x] = 1
+            if map_[y][i] == -1 and map_[i][x] == -1:
+                map_[y][x] = -1
+
+count = int(sys.stdin.readline())
+
+for _ in range(count):
+    start, end = map(int, sys.stdin.readline().split())
+    print(map_[start - 1][end - 1])
