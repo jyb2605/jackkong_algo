154ae539b60948142312acf2b3e48a809aa5a367
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-08-15 15:52:53 +0900

    party

--- 20200815/jaeyeonlee0621_파티.py
+++ 20200815/jaeyeonlee0621_파티.py
@@ -1,29 +1,25 @@
 import sys
 
 n, m, goal = map(int, sys.stdin.readline().split())
+goal = goal - 1
+
 
 default = 100_000_000
-go = [[default for _ in range(n)] for _ in range(n)]
-back = [[default for _ in range(n)] for _ in range(n)]
+map_ = [[default for _ in range(n)] for _ in range(n)]
 
 for _ in range(m):
     start, end, weight = map(int, sys.stdin.readline().split())
-    go[start - 1][end - 1] = weight
-    back[start - 1][end - 1] = weight
+    map_[start - 1][end - 1] = weight
 
 for i in range(n):
     for x in range(n):
-        if x != i and goal != i:
-            go[x][goal] = min(go[x][goal], go[x][i] + go[i][goal])
-
-for i in range(n):
-    for x in range(n):
-        if x != i and goal != i:
-            back[goal][x] = min(back[goal][x], back[goal][i] + back[i][x])
+        for y in range(n):
+            if x != y:
+                map_[x][y] = min(map_[x][y], map_[x][i] + map_[i][y])
 
 answer = 0
 for i in range(n):
-    if go[x][goal] != default and back[goal][x] != default:
-        answer = max(answer, go[x][goal] + back[goal][x])
+    if i != goal:
+        answer = max(answer, map_[i][goal] + map_[goal][i])
 
 print(answer)
