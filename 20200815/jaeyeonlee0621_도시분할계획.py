eed82ec528b9962436cff55e6df08e55352d0bf0
JaeYeonLee0621 <ljyeon9466@gmail.com>
2020-08-16 13:02:43 +0900

    memoery exceed

---
+++ 20200815/jaeyeonlee0621_도시분할계획.py
@@ -0,0 +1,50 @@
+import sys
+from heapq import heappop, heappush
+
+
+def bfs(start, end, node, visited):
+    visited_ = [[0 for _ in range(node)] for _ in range(node)]
+    vertex = [0 for _ in range(node)]
+    vertex[start] = 1
+
+    queue = []
+    heappush(queue, [start, end])
+
+    while queue:
+        start, end = heappop(queue)
+
+        if vertex[end]:
+            return True
+
+        vertex[end] = 1
+        visited_[start][end] = 1
+        visited_[end][start] = 1
+
+        for i in range(node):
+            if visited[end][i] and not visited_[end][i]:
+                heappush(queue, [end, i])
+
+    return False
+
+
+node, count = map(int, sys.stdin.readline().split())
+
+visited = [[0 for _ in range(node)] for _ in range(node)]
+answer = []
+vertexes = []
+
+for _ in range(count):
+    start, end, weight = map(int, sys.stdin.readline().split())
+    vertexes.append([weight, start - 1, end - 1])
+
+vertexes.sort(key=lambda vertexes: vertexes[0])
+
+for weight, start, end in vertexes:
+    print(f'{start} {end}')
+    print(bfs(start, end, node, visited))
+    if not visited[start][end] and not bfs(start, end, node, visited):
+        visited[start][end] = 1
+        visited[end][start] = 1
+        answer.append(weight)
+
+print(sum(answer[:-1]))
