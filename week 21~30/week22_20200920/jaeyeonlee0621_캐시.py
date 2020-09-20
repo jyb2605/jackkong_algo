c595508d0a6549a0c7270965dee820dd80a7d9b7
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-09-20 16:20:33 +0900

    example

--- week 21~30/week22_20200920/jaeyeonlee0621_캐시.py
+++ week 21~30/week22_20200920/jaeyeonlee0621_캐시.py
@@ -16,12 +16,12 @@ def solution(cacheSize, cities):
     return answer
 
 
-# print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
 print(solution(3, ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
-# print(solution(3, ["Jeju", "Pangyo", "Jeju", "Jeju"]))
-# print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju",
-#                    "NewYork", "Rome"]))
-# print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju",
-#                    "NewYork", "Rome"]))
-# print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
-# print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3, ["Jeju", "Pangyo", "Jeju", "Jeju"]))
+print(solution(2, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju",
+                   "NewYork", "Rome"]))
+print(solution(5, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju",
+                   "NewYork", "Rome"]))
+print(solution(2, ["Jeju", "Pangyo", "NewYork", "newyork"]))
+print(solution(0, ["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
