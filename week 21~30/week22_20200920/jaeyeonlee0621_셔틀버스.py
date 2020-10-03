a0cb8d166e5c825aacb7189045d1457f70f101df
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-10-03 11:18:44 +0900

    remove useless memory

--- week 21~30/week22_20200920/jaeyeonlee0621_셔틀버스.py
+++ week 21~30/week22_20200920/jaeyeonlee0621_셔틀버스.py
@@ -8,19 +8,19 @@ def solution(n, t, m, timetable):
         heapq.heappush(crew, 60 * int(hour) + int(minute))
 
     bus_time = 9 * 60
-    bus = {}
+    last_time = 9 * 60 + (n - 1) * t
+    bus = []
     for _ in range(n):
-        bus[bus_time] = []
-        while crew and len(bus[bus_time]) < m:
-            crew_time = crew[0]
-            if crew_time > bus_time:
-                break
-            bus[bus_time].append(heapq.heappop(crew))
+        count = 0
+        while crew and count < m and crew[0] <= bus_time:
+            count += 1
+            crew_time = heapq.heappop(crew)
+            if bus_time == last_time:
+                bus.append(crew_time)
         bus_time += t
 
-    last_time = 9 * 60 + (n - 1) * t
-    if len(bus[last_time]) == m:
-        last_time = bus[last_time][-1] - 1
+    if len(bus) == m:
+        last_time = bus[-1] - 1
 
     return f'{str(int(last_time / 60)).zfill(2)}:{str(int(last_time % 60)).zfill(2)}'
 
