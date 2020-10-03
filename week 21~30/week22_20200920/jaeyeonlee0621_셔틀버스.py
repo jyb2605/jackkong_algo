ec9f402bd6158e8a8bfe875002c15461dd103df0
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-10-03 11:38:45 +0900

    remove useless list

--- week 21~30/week22_20200920/jaeyeonlee0621_셔틀버스.py
+++ week 21~30/week22_20200920/jaeyeonlee0621_셔틀버스.py
@@ -7,20 +7,19 @@ def solution(n, t, m, timetable):
         hour, minute = timetable[index].split(':')
         heapq.heappush(crew, 60 * int(hour) + int(minute))
 
-    bus_time = 9 * 60
-    last_time = 9 * 60 + (n - 1) * t
-    bus = []
+    bus_time, last_time = 9 * 60, 9 * 60 + (n - 1) * t
+    crew_last_time, count = 0, 0
     for _ in range(n):
         count = 0
         while crew and count < m and crew[0] <= bus_time:
             count += 1
             crew_time = heapq.heappop(crew)
             if bus_time == last_time:
-                bus.append(crew_time)
+                crew_last_time = crew_time
         bus_time += t
 
-    if len(bus) == m:
-        last_time = bus[-1] - 1
+    if count == m:
+        last_time = crew_last_time - 1
 
     return f'{str(int(last_time / 60)).zfill(2)}:{str(int(last_time % 60)).zfill(2)}'
 
