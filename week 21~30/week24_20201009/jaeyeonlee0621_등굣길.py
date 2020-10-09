643136f42a6c705450edd1cf10a48508717f91e5
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-10-09 21:41:02 +0900

    시바

--- week 21~30/week24_20201009/jaeyeonlee0621_등굣길.py
+++ week 21~30/week24_20201009/jaeyeonlee0621_등굣길.py
@@ -1,1 +1,20 @@
 # 최단 경로 순열
+
+def solution(m, n, puddles):
+    map = [[1 for _ in range(m)] for _ in range(n)]
+    map[n - 1][m - 1] = -1
+
+    for start, end in puddles:
+        map[start - 1][end - 1] = 0
+
+    for y in range(1, n):
+        for x in range(1, m):
+            if map[y][x] == 0:
+                continue
+            map[y][x] = map[y - 1][x] + map[y][x - 1]
+            
+    return 0 if map[n - 1][m - 1] == -1 else map[n - 1][m - 1]
+
+
+print(solution(4, 3, [[2, 2]]))
+print(solution(4, 3, [[1, 3], [3, 1]]))
