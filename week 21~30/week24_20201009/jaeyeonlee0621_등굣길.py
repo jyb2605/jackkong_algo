97ed563f76d0ed386fe59ccb2405a6c549fb8d72
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-10-09 23:48:24 +0900

    solve..

--- week 21~30/week24_20201009/jaeyeonlee0621_등굣길.py
+++ week 21~30/week24_20201009/jaeyeonlee0621_등굣길.py
@@ -1,20 +1,23 @@
 # 최단 경로 순열
 
 def solution(m, n, puddles):
-    map = [[1 for _ in range(m)] for _ in range(n)]
-    map[n - 1][m - 1] = -1
+    map = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
     for start, end in puddles:
-        map[end - 1][start - 1] = 0
+        map[end][start] = -1
 
-    for y in range(1, n):
-        for x in range(1, m):
-            if map[y][x] == 0:
+    map[1][1] = 1
+    for y in range(1, n + 1):
+        for x in range(1, m + 1):
+            if x == y == 1:
+                continue
+            if map[y][x] == -1:
+                map[y][x] = 0
                 continue
             map[y][x] = (map[y - 1][x] + map[y][x - 1]) % 1000000007
 
-    return 0 if map[n - 1][m - 1] == -1 else map[n - 1][m - 1]
+    return map[n][m]
 
 
-print(solution(4, 3, [[2, 2]]))
+# print(solution(4, 3, [[2, 2]]))
 print(solution(4, 3, [[1, 3], [3, 1]]))
