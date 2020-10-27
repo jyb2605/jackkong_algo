45939f3252b249ef319b6b1ba8672606b1e91dd5
momojh94 <mjh79017901@gmail.com>
2020-10-28 00:41:15 +0900

    Fix week 24 레드 블루 스패닝 트리
    소스 코드 다른 문제 코드로 잘 못 복사함.

--- week 21~30/week24_20201017/momojh94_레드 블루 스패닝 트리.java
+++ week 21~30/week24_20201017/momojh94_레드 블루 스패닝 트리.java
@@ -8,7 +8,7 @@
  * 스패닝트리 블루 최소 갯수 <= k <= 최대 갯수 이면 return 1 아니면 return 0
  * 
  * 첫 제출 때 틀렸는데 union 함수 잘 못 구현함.
- *  public void union(int[] parents, int x, int y) {
+ * public void union(int[] parents, int x, int y) {
         x = find(parents, x);
         y = find(parents, y);
         if(x != y) {
@@ -18,8 +18,6 @@
  * parent[y] = x 로 해야되는데 y = x;로 해서 틀렸음.
  */
 
-package com.company;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -28,114 +26,101 @@ import java.io.OutputStreamWriter;
 import java.util.*;
 
 class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = null;
         Solution s = new Solution();
         int answer = 0;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int a, b, c;
-
-        Edge[] edges = new Edge[m + 1];
-        for (int idx = 0; idx < m + 1; idx++) {
+        String n;
+        char c;
+        int m, k, f, t;
+        Edge[] edges;
+        while(true) {
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-            edges[idx] = new Edge(a, b, c);
+            n = st.nextToken();
+            m = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            if(n.equals("0")) {
+                break;
+            } else {
+                edges = new Edge[m];
+                for(int idx = 0; idx < m; idx++) {
+                    st = new StringTokenizer(br.readLine());
+                    c = st.nextToken().charAt(0);
+                    f = Integer.parseInt(st.nextToken());
+                    t = Integer.parseInt(st.nextToken());
+                    edges[idx] = new Edge(f, t, (c =='R') ? 0 : 1);
+                }
+                answer = s.solution(edges, Integer.parseInt(n), k);
+                bw.write(Integer.toString(answer));
+                bw.newLine();
+            }
         }
-        answer = s.solution(n, m, edges);
-        bw.write(Integer.toString(answer));
-        bw.newLine();
+
         bw.flush();
         bw.close();
     }
 }
 
-
 class Edge {
-    int[] node;
-    int cost;
-    public Edge(int a, int b, int c) {
-        this.node = new int[]{a, b};
-        this.cost = c;
+    int node1;
+    int node2;
+    int color;
+
+    public Edge(int node1, int node2, int color) {
+        this.node1 = node1;
+        this.node2 = node2;
+        this.color = color;
     }
 }
 
-// 0 오르막길
-// 1 내리막길
-
 class Solution {
-    public int solution(int n, int m, Edge[] edges) {
-        int answer = 0;
-        int max = 0;
-        int min = 0;
+    public int solution(Edge[] edges, int n, int k) {
+        int ret = 0;
+        int redPriorityBlueEdgesCnt = 0;
+        int bluePriorityBlueEdgesCnt = 0;
 
-        Comparator<Edge> uphillPriorityComp = new Comparator<Edge>() {
-            @Override
-            public int compare(Edge o1, Edge o2) {
-                return o1.cost - o2.cost;
-            }
-        };
-
-        Comparator<Edge> downhillPriorityComp = new Comparator<Edge>() {
-            @Override
-            public int compare(Edge o1, Edge o2) {
-                return o2.cost - o1.cost;
-            }
-        };
-
-        Edge[] sortedEdges = new Edge[edges.length];
+        // Red 우선
+        Arrays.sort(edges, (o1, o2) -> o1.color - o2.color);
         int[] parents = new int[n + 1];
-
-        // 오르막길 우선
-        for(int idx = 0; idx < edges.length; idx++) {
-            sortedEdges[idx] = edges[idx];
-        }
-        Arrays.sort(sortedEdges, uphillPriorityComp);
-
-        for(int idx = 0; idx < n + 1; idx++) {
+        for(int idx = 1; idx < n + 1; idx++) {
             parents[idx] = idx;
         }
-        int selectedEdgesCnt = 0;
-
-        for(Edge edge : sortedEdges) {
-            if(selectedEdgesCnt >= n) break;
-            if(find(parents, edge.node[0]) != find(parents, edge.node[1])) {
-                if(edge.cost == 0) {
-                    max++;
+        int treeCnt = 0;
+        for(Edge e : edges) {
+            if(treeCnt >= n - 1) break;
+            if(find(parents, e.node1) != find(parents, e.node2)) {
+                union(parents, e.node1, e.node2);
+                treeCnt++;
+                if(e.color == 1) {
+                    redPriorityBlueEdgesCnt++;
                 }
-                union(parents, edge.node[0], edge.node[1]);
-                selectedEdgesCnt++;
             }
         }
 
-        // 내리막길 우선
-        for(int idx = 0; idx < edges.length; idx++) {
-            sortedEdges[idx] = edges[idx];
-        }
-        Arrays.sort(sortedEdges, downhillPriorityComp);
-        for(int idx = 0; idx < n + 1; idx++) {
+        // Blue 우선
+        Arrays.sort(edges, (o1, o2) -> o2.color - o1.color);
+        for(int idx = 1; idx < n + 1; idx++) {
             parents[idx] = idx;
         }
-
-        selectedEdgesCnt = 0;
-        for(Edge edge : sortedEdges) {
-            if(selectedEdgesCnt >= n) break;
-            if(find(parents, edge.node[0]) != find(parents, edge.node[1])) {
-                if(edge.cost == 0) {
-                    min++;
+        treeCnt = 0;
+        for(Edge e : edges) {
+            if(treeCnt >= n - 1) break;
+            if(find(parents, e.node1) != find(parents, e.node2)) {
+                union(parents, e.node1, e.node2);
+                treeCnt++;
+                if(e.color == 1) {
+                    bluePriorityBlueEdgesCnt++;
                 }
-                union(parents, edge.node[0], edge.node[1]);
-                selectedEdgesCnt++;
             }
         }
 
-        answer = max * max - min * min;
-        return answer;
+        ret = (redPriorityBlueEdgesCnt <= k && k <= bluePriorityBlueEdgesCnt)
+                ? 1
+                : 0;
+        return ret;
     }
 
     public void union(int[] parents, int x, int y) {
