0a272eb3b382eecc75299d1e9c93551ae8c0d198
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-11-01 16:23:51 +0900

    startswith

--- week 21~30/week26_20201101/jaeyeonlee0621_전화번호목록.py
+++ week 21~30/week26_20201101/jaeyeonlee0621_전화번호목록.py
@@ -3,7 +3,7 @@ import sys
 
 def solution(phone_book):
     for i in range(len(phone_book) - 1):
-        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+        if phone_book[i + 1].startswith(phone_book[i]):
             return False
     return True
 
