237327a730ed14b1524185d0cab999c5c408b238
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-11-01 16:22:36 +0900

    전화번호목록

--- week 21~30/week26_20201101/jaeyeonlee0621_전화번호목록.py
+++ week 21~30/week26_20201101/jaeyeonlee0621_전화번호목록.py
@@ -1,7 +1,18 @@
 import sys
 
+
+def solution(phone_book):
+    for i in range(len(phone_book) - 1):
+        if phone_book[i] == phone_book[i + 1][:len(phone_book[i])]:
+            return False
+    return True
+
+
 n = int(sys.stdin.readline())
 for _ in range(n):
+    phone_book = []
     m = int(sys.stdin.readline())
     for _ in range(m):
-        number = str(sys.stdin.readline())
+        phone_book.append(str(sys.stdin.readline())[:-1])
+    phone_book.sort()
+    print('YES' if solution(phone_book) else 'NO')
