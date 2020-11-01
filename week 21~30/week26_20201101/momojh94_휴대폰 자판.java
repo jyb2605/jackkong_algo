0e117b49a5a78ac8efc41910d73799192c96b112
momojh94 <mjh79017901@gmail.com>
2020-11-01 17:57:53 +0900

    Improve week 26 휴대폰 자판
    input 방법 수정으로 걸린 시간 개선
    Scanner -> BufferedReader
    2108ms -> 1068ms

--- week 21~30/week26_20201101/momojh94_휴대폰 자판.java
+++ week 21~30/week26_20201101/momojh94_휴대폰 자판.java
@@ -10,23 +10,28 @@
  * 2일 경우 2.0으로 나오게하면 틀리는 듯 로직 수정 안하고 그대로 하고
  * 출력 포맷만 바꾸니 정답 (System.out.format으로 2.00 나오게하니 정답.)
  * 
- * 들어오는 예제 입력을 보면 eof 처리 해서 while에서 탈출하게 해야 런타임 에러가 안뜸. 
+ * 들어오는 예제 입력을 보면 eof 처리 해서 while에서 탈출하게 해야 런타임 에러가 안뜸.
+ *
+ * Scanner -> BufferedReader 입력 방법 수정하니 빨라졌음.
+ * 2108ms -> 1068ms
  */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Solution s = new Solution();
-        int n = 0;
-        while(sc.hasNextInt()) {
-            n = sc.nextInt();
+        String input;
+        while((input = br.readLine()) != null) {
+            int n = Integer.parseInt(input);
             String[] words = new String[n];
             for(int idx = 0; idx < n; idx++) {
-                words[idx] = sc.next();
+                words[idx] = br.readLine();
             }
             System.out.format("%.2f\n", (float) s.solution(words) / n);
         }
@@ -101,7 +106,6 @@ class Trie {
 class Solution {
     public int solution(String[] words) {
         int ret = 0;
-        int n = words.length;
         Trie trie = new Trie();
         for(String word : words) {
             trie.add(word);
