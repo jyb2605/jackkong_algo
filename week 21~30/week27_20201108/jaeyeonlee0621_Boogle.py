b09108affb97094b0facb67147a061503d3f13c0
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-11-08 17:06:32 +0900

    시간초과

--- week 21~30/week27_20201108/jaeyeonlee0621_Boogle.py
+++ week 21~30/week27_20201108/jaeyeonlee0621_Boogle.py
@@ -1,50 +1,6 @@
 import sys
 
 
-def search(y, x, node):
-    next_node = node.next.get(boards[y][x])
-    stack = [] if not next_node else [(y, x, next_node, boards[y][x])]
-    answer = []
-
-    visited = [[0 for _ in range(4)] for _ in range(4)]
-    while stack:
-        y, x, node, word = stack.pop()
-
-        if words.get(word):
-            answer.append(word)
-
-        if visited[y][x] == 1:
-            continue
-
-        visited[y][x] = 1
-
-        if y - 1 >= 0 and node.next.get(boards[y - 1][x]):
-            stack.append((y - 1, x, node.next.get(boards[y - 1][x]), word + boards[y - 1][x]))
-
-        if x - 1 >= 0 and node.next.get(boards[y][x - 1]):
-            stack.append((y, x - 1, node.next.get(boards[y][x - 1]), word + boards[y][x - 1]))
-
-        if y + 1 < 4 and node.next.get(boards[y + 1][x]):
-            stack.append((y + 1, x, node.next.get(boards[y + 1][x]), word + boards[y + 1][x]))
-
-        if x + 1 < 4 and node.next.get(boards[y][x + 1]):
-            stack.append((y, x + 1, node.next.get(boards[y][x + 1]), word + boards[y][x + 1]))
-
-        if y - 1 >= 0 and x - 1 >= 0 and node.next.get(boards[y - 1][x - 1]):
-            stack.append((y - 1, x - 1, node.next.get(boards[y - 1][x - 1]), word + boards[y - 1][x - 1]))
-
-        if y - 1 >= 0 and x + 1 < 4 and node.next.get(boards[y - 1][x + 1]):
-            stack.append((y - 1, x + 1, node.next.get(boards[y - 1][x + 1]), word + boards[y - 1][x + 1]))
-
-        if y + 1 < 4 and x - 1 >= 0 and node.next.get(boards[y + 1][x - 1]):
-            stack.append((y + 1, x - 1, node.next.get(boards[y + 1][x - 1]), word + boards[y + 1][x - 1]))
-
-        if y + 1 < 4 and x + 1 < 4 and node.next.get(boards[y + 1][x + 1]):
-            stack.append((y + 1, x + 1, node.next.get(boards[y + 1][x + 1]), word + boards[y + 1][x + 1]))
-
-    return answer
-
-
 class Node:
     def __init__(self, char):
         self.char = char
@@ -54,12 +10,27 @@ class Node:
         return f'{self.char} {self.next}'
 
 
+def search(y, x, node, word, visited):
+    if word and words.get(word):
+        answer.append(word)
+    if visited[y][x] == 1:
+        return
+    visited[y][x] = 1
+    for x_move, y_move in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
+        y_, x_ = y + y_move, x + x_move
+        if 0 <= x_ < 4 and 0 <= y_ < 4 and node.next.get(boards[y_][x_]):
+            search(y_, x_, node.next.get(boards[y_][x_]), word + boards[y_][x_], visited)
+    visited[y][x] = 0
+
+
+# word 입력
 word_count = int(sys.stdin.readline())
 words = {}
 for _ in range(word_count):
     words[str(sys.stdin.readline().strip())] = 1
 sys.stdin.readline()
 
+# tree 만들기
 root_node = Node(char=None)
 for word in words:
     present_node = root_node
@@ -73,17 +44,28 @@ for i in range(board_count):
     answers = [0, '', 0]
     find_word = {}
 
+    # board 입력
     boards = [str(sys.stdin.readline().strip()) for _ in range(4)]
     if i != board_count - 1:
         sys.stdin.readline()
 
     for y in range(4):
         for x in range(4):
-            answer = search(y, x, root_node)
+            node = root_node.next.get(boards[y][x])
+            if not node:
+                continue
 
+            # search
+            answer = []
+            visited = [[0 for _ in range(4)] for _ in range(4)]
+
+            search(y, x, node, boards[y][x], visited)
             for word in answer:
-                if find_word.get(word):
+                if not find_word.get(word[0]):
+                    find_word[word[0]] = []
+                if word in find_word[word[0]]:
                     continue
+                find_word[word[0]].append(word)
 
                 find_word[word] = 1
                 answers[2] += 1
@@ -100,7 +82,7 @@ for i in range(board_count):
                 elif len_word == 8:
                     answers[0] += 11
 
-                if len(answers[1]) < len(word) or (len(answers[1]) == len(word) and answers[1][0] > word[0]):
+                if len(answers[1]) < len(word) or (len(answers[1]) == len(word) and answers[1] > word):
                     answers[1] = word
 
     print(f'{answers[0]} {answers[1]} {answers[2]}')
