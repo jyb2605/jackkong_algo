b8562845b3d181e735570e73c5b3f82c5f0345f6
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-11-08 19:44:45 +0900

    헤헤 이름이 잘못됬네

--- week 21~30/week27_20201108/jaeyeonlee0621_Boggle.py
+++ week 21~30/week27_20201108/jaeyeonlee0621_Boggle.py
@@ -5,23 +5,22 @@ class Node:
     def __init__(self, char):
         self.char = char
         self.next = {}
+        self.is_final = False
 
     def __repr__(self):
-        return f'{self.char} {self.next}'
+        return f'{self.char} {self.next} {self.is_final}'
 
 
 def dfs(y, x, word, visited, node):
-    answer[word] = 1
-
+    if node.is_final:
+        answer[word] = 1
     if len(word) == 8:
         return
-
     for x_move, y_move in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
         y_, x_ = y + y_move, x + x_move
-        if 0 <= x_ < 4 and 0 <= y_ < 4 and visited[y_][x_] == 0:
+        if 0 <= x_ < 4 and 0 <= y_ < 4 and not visited[y_][x_] and node.next.get(boards[y_][x_]):
             visited[y_][x_] = 1
-            if node.next.get(boards[y_][x_]):
-                dfs(y_, x_, word + boards[y_][x_], visited, node.next.get(boards[y_][x_]))
+            dfs(y_, x_, word + boards[y_][x_], visited, node.next.get(boards[y_][x_]))
             visited[y_][x_] = 0
 
 
@@ -40,10 +39,11 @@ for word in words.keys():
         if present_node.next.get(character) is None:
             present_node.next[character] = Node(char=character)
         present_node = present_node.next[character]
+    present_node.is_final = True
 
 board_count = int(sys.stdin.readline())
 for i in range(board_count):
-    answers = [0, '', 0]
+    point, longest_word, count = 0, '', 0
     answer = {}
 
     # board 입력
@@ -51,22 +51,23 @@ for i in range(board_count):
     if i != board_count - 1:
         sys.stdin.readline()
 
+    # dfs
     visited = [[0 for _ in range(4)] for _ in range(4)]
     for y in range(4):
         for x in range(4):
-            if root_node.next.get(boards[y][x]):
-                visited[y][x] = 1
-                dfs(y, x, boards[y][x], visited, root_node.next[boards[y][x]])
-                visited[y][x] = 0
+            if not root_node.next.get(boards[y][x]):
+                continue
+            visited[y][x] = 1
+            dfs(y, x, boards[y][x], visited, root_node.next[boards[y][x]])
+            visited[y][x] = 0
 
+    # 점수 계산
     score = {3: 1, 4: 1, 5: 2, 6: 3, 7: 5, 8: 11}
     for word in answer.keys():
-        if not words.get(word):
-            continue
-        answers[2] += 1
+        count += 1
         if score.get(len(word)):
-            answers[0] += score[len(word)]
-        if len(answers[1]) < len(word) or (len(answers[1]) == len(word) and answers[1] > word):
-            answers[1] = word
+            point += score[len(word)]
+        if len(longest_word) < len(word) or (len(longest_word) == len(word) and longest_word > word):
+            longest_word = word
 
-    print(f'{answers[0]} {answers[1]} {answers[2]}')
+    print(f'{point} {longest_word} {count}')
