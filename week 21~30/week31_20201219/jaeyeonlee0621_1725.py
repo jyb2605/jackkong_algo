76a9922c1bc461d19bed5bd5abf4fb91978f9d91
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-12-19 23:22:41 +0900

    ?

--- week 21~30/week31_20201219/jaeyeonlee0621_1725.py
+++ week 21~30/week31_20201219/jaeyeonlee0621_1725.py
@@ -3,18 +3,18 @@ import sys
 n = int(sys.stdin.readline().strip())
 graph = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
-answer = 0
-
+answer, min_width = 0, 2000000000
 stack = []
 for i in range(n):
+    min_width = min(min_width, graph[i])
     while stack and stack[-1][1] > graph[i]:
         index, width = stack.pop()
         answer = max((i - index) * width, answer)
     stack.append((i, graph[i]))
 
-index_ = stack[-1][0] + 1
 while stack:
     index, width = stack.pop()
-    answer = max((index_ - index) * width, answer)
+    answer = max((n - index) * width, answer)
 
+answer = max(answer, min_width * n)
 print(answer)
