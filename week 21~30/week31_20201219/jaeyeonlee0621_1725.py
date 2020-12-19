1c429d4ea7cf3458edd417f3c38cd09c56ff6f9c
jaeyeonlee0621 <ljyeon9466@gmail.com>
2020-12-19 23:34:52 +0900

    width 설정

--- week 21~30/week31_20201219/jaeyeonlee0621_1725.py
+++ week 21~30/week31_20201219/jaeyeonlee0621_1725.py
@@ -3,18 +3,18 @@ import sys
 n = int(sys.stdin.readline().strip())
 graph = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
-answer, min_width = 0, 2000000000
+answer = 0
 stack = []
 for i in range(n):
-    min_width = min(min_width, graph[i])
-    while stack and stack[-1][1] > graph[i]:
-        index, width = stack.pop()
-        answer = max((i - index) * width, answer)
-    stack.append((i, graph[i]))
+    while stack and graph[stack[-1]] > graph[i]:
+        index = stack.pop()
+        width = i - stack[-1] - 1 if stack else i
+        answer = max(width * graph[index], answer)
+    stack.append(i)
 
 while stack:
-    index, width = stack.pop()
-    answer = max((n - index) * width, answer)
+    index = stack.pop()
+    width = n - stack[-1] - 1 if stack else n
+    answer = max(width * graph[index], answer)
 
-answer = max(answer, min_width * n)
 print(answer)
