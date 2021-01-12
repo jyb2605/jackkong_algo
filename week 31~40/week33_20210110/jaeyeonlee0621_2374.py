6011daeb94f97ce57721151b602403e69589ae14
jaeyeonlee0621 <ljyeon9466@gmail.com>
2021-01-12 22:58:07 +0900

    ..?

--- week 31~40/week33_20210110/jaeyeonlee0621_2374.py
+++ week 31~40/week33_20210110/jaeyeonlee0621_2374.py
@@ -1,24 +1,22 @@
 import sys
 
-n = int(sys.stdin.readline())
-queue = [int(sys.stdin.readline()) for _ in range(n)]
-
 answer = 0
 
-while len(queue) > 1:
-    unique_set = set()
-    queue = [x for x in queue if not (x in unique_set or unique_set.add(x))]
+n = int(sys.stdin.readline())
+queue = []
 
-    for i in range(len(queue)):
-        if i == 0 and i + 1 < len(queue) and queue[i] < queue[i + 1]:
-            answer += queue[i + 1] - queue[i]
-            queue[i] = queue[i + 1]
-        if i == len(queue) - 1 and i - 1 >= 0 and queue[i - 1] > queue[i]:
-            answer += queue[i - 1] - queue[i]
-            queue[i] = queue[i - 1]
-        if i - 1 >= 0 and i + 1 < len(queue) and queue[i - 1] > queue[i] < queue[i + 1]:
-            min_queue = min(queue[i - 1], queue[i + 1])
-            answer += min_queue - queue[i]
-            queue[i] = min_queue
+for i in range(n):
+    num = int(sys.stdin.readline())
 
+    if not queue or queue[-1] > num:
+        queue.append(num)
+        continue
+
+    answer += num - queue[-1]
+    while queue or queue[-1] < num:
+        queue.pop()
+
+    queue.append(num)
+
+answer += queue[0] - queue[-1]
 print(answer)
